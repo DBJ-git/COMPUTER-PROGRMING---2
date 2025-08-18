9a19a76af72ee18ef3d799b89e41f8fb108bc844
DBJ-git <divyjoshi.9999@gmail.com>
2025-08-19 00:21:36 +0530

    Update result.c

--- result.c
+++ result.c
@@ -11,10 +11,12 @@ printf("Enter the marks of chemistry : ");
 scanf("%d" , &chemistry);
 printf("Enter the marks of physics : ");
 scanf("%d" , &physics);
-
-int avg = (maths + chemistry + physics)/3;
-
-printf("Average of three subject = %d \n",avg);
+  
+int total  = maths + chemistry + physics;
+float avg = (maths + chemistry + physics)/3;
+  
+printf("Total of three subject = %d\n",total);
+printf("Average of three subject = %f \n",avg);
 
 if(maths < 35 || chemistry < 35 || physics < 35){
 printf("Better luck next time ! You are fail.\n");
